895ee1d331da2b289a4fc8223c4db957e9b0eee9
hagiape <g_chrisf@protonmail.com>
2023-04-29 13:29:39 +0800

    added pseudocode

--- simple_calculator.py
+++ simple_calculator.py
@@ -6,3 +6,26 @@
 # 5. If yes, repeat Step 1.
 # 6. If no, Display “Thank you!” and the program will exit 
 # 7. Use Python Function and appropriate Exceptions to capture errors during runtime.
+
+# try block
+# while loop
+# ask operation to be used from user
+# initialize two variables from input
+# first number = input
+# second number = input
+# perform operation if else
+# if +:
+# add two variables
+# if -:
+# subtract two varibles
+# if *:
+# multiply two variables
+# if /:
+# divide two variables
+# display output
+# ask if user wants to try again
+# if no break loop
+# print 'Thank you'
+# if neither yes or no break loop
+# raise exception 'please answer only yes or no'
+# implement except block
