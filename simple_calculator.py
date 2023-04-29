f43f76644d3d8bb0bf55dc66826f51cb95cd2f17
hagiape <g_chrisf@protonmail.com>
2023-04-29 20:50:02 +0800

    removed pseudocode

--- simple_calculator.py
+++ simple_calculator.py
@@ -7,57 +7,34 @@
 # 6. If no, Display “Thank you!” and the program will exit 
 # 7. Use Python Function and appropriate Exceptions to capture errors during runtime.
 
-# initialize public variables
 operation = ''
 first_number = ''
 second_number = ''
 result = ''
-print('Welcome to a simple calculator program.')
-# try block
+print('Welcome to a simple calculator program')
 try:
-# while loop
     while True:
-# ask operation to be used from user
         operation = input('"+" for addition\n"-" for subtraction\n"x" for multiplication\n"/" for division\nWhat operation would you like to be performed? ')
-# first number = input
         first_number = input('What is the first number? ')
-# second number = input
-        second_number = input('What is the second number?' )
-# perform operation if else
+        second_number = input('What is the second number? ' )
         match operation:
-        # if +:
             case '+':
-                # add two variables
                 result = int(first_number) + int(second_number)
-        # if -:
             case '-':
-                # subtract two varibles
                 result = int(first_number) - int(second_number)
-        # if *:
             case '*':
-                # multiply two variables
                 result = int(first_number) * int(second_number)
-        # if /:
             case '/':
-            # divide two variables
                 result = int(first_number) / int(second_number)
-# display output
         print(result)
-        # ask if user wants to try again
         prompt_answer = input('Would you like to perform another calculation?\n(answer with "yes" or "no" only:' )
         prompt_answer.lower()
-            # if yes
-            # if no break loop
         if prompt_answer == 'no':
-            # print 'Thank you'
             print('Thank you for using the program!')
             break
-            # if neither yes or no, break loop
-        # raise exception 'please answer only yes or no'
         else:
             if prompt_answer != 'yes':
                 raise Exception('Please answer with "yes" or "no" only.')
-# implement except block
 except ZeroDivisionError:
     print('You cannot divide by zero!')
 except ValueError:
