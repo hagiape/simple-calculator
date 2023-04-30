5006d56effe2d1c6808bbd79591d9cdda1d11e74
hagiape <g_chrisf@protonmail.com>
2023-04-30 19:15:56 +0800

    implemented design changes

--- simple_calculator.py
+++ simple_calculator.py
@@ -7,6 +7,18 @@
 # 6. If no, Display “Thank you!” and the program will exit 
 # 7. Use Python Function and appropriate Exceptions to capture errors during runtime.
 
+# implemented code from Stack Overflow
+# https://stackoverflow.com/a/20757225
+# function that creates text border based on the output
+def border(string):
+    text = string.splitlines()
+    max_length = max(len(s) for s in text)
+    column_width = max_length + 2
+    print('+' + ('-' * column_width) + '+')
+    for s in text:
+        print('| %-*.*s |' % (max_length, max_length, s))
+    print('+' + ('-' * column_width) + '+')
+
 operation = ''
 first_number = ''
 second_number = ''
@@ -26,17 +38,16 @@ try:
             case '*':
                 result = int(first_number) * int(second_number)
             case '/':
-                result = int(first_number) / int(second_number)
+                result = float(first_number) / float(second_number)
             case _:
                 raise Exception('Please enter only the mentioned operators.')
-        print(int(result))
+        border('The answer is: ' + str(result))
         prompt_answer = input('Would you like to perform another calculation?\nanswer with "yes" or "no" only: ' )
         prompt_answer = prompt_answer.lower()
         if prompt_answer == 'no':
             print('Thank you for using the program!')
             break
-        else:
-            if prompt_answer != 'yes':
+        elif prompt_answer != 'yes':
                 raise Exception('Please answer with "yes" or "no" only.')
 except ZeroDivisionError:
     print('You cannot divide by zero!')
